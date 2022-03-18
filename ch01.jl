268b9d43646702a49cd6e3256abb79586296f776
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-03-18 13:48:26 +0100

    fix case in plot

--- ch01.jl
+++ ch01.jl
@@ -48,16 +48,16 @@ DataFrame(a=1:3, name=["Alice", "Bob", "Clyde"],
 
 data = """
 problem,language,time,size
-n-body,c,2.13,1633
-mandelbrot,c,1.3,1135
-spectral norm,c,0.41,1197
-fannkuch-redux,c,7.58,910
-fasta,c,0.78,1463
-k-nucleotide,c,3.96,1506
-binary-trees,c,1.58,809
-reverse-complement,c,0.41,1965
-pidigits,c,0.56,1090
-regex-redux,c,0.8,1397
+n-body,C,2.13,1633
+mandelbrot,C,1.3,1135
+spectral norm,C,0.41,1197
+fannkuch-redux,C,7.58,910
+fasta,C,0.78,1463
+k-nucleotide,C,3.96,1506
+binary-trees,C,1.58,809
+reverse-complement,C,0.41,1965
+pidigits,C,0.56,1090
+regex-redux,C,0.8,1397
 n-body,Java,6.77,1489
 mandelbrot,Java,4.1,796
 spectral norm,Java,1.55,756
@@ -68,26 +68,26 @@ binary-trees,Java,2.51,835
 reverse-complement,Java,1.57,2183
 pidigits,Java,0.79,764
 regex-redux,Java,5.34,929
-n-body,python,541.34,1196
-mandelbrot,python,177.35,688
-spectral norm,python,112.97,407
-fannkuch-redux,python,341.45,950
-fasta,python,36.9,1947
-k-nucleotide,python,46.31,1967
-binary-trees,python,44.7,660
-reverse-complement,python,6.62,814
-pidigits,python,1.16,567
-regex-redux,python,1.34,1403
-n-body,julia,4.21,1111
-mandelbrot,julia,1.42,619
-spectral norm,julia,1.11,429
-fannkuch-redux,julia,7.83,1067
-fasta,julia,1.13,1082
-k-nucleotide,julia,4.94,951
-binary-trees,julia,7.28,634
-reverse-complement,julia,1.44,522
-pidigits,julia,0.97,506
-regex-redux,julia,1.74,759
+n-body,Python,541.34,1196
+mandelbrot,Python,177.35,688
+spectral norm,Python,112.97,407
+fannkuch-redux,Python,341.45,950
+fasta,Python,36.9,1947
+k-nucleotide,Python,46.31,1967
+binary-trees,Python,44.7,660
+reverse-complement,Python,6.62,814
+pidigits,Python,1.16,567
+regex-redux,Python,1.34,1403
+n-body,Julia,4.21,1111
+mandelbrot,Julia,1.42,619
+spectral norm,Julia,1.11,429
+fannkuch-redux,Julia,7.83,1067
+fasta,Julia,1.13,1082
+k-nucleotide,Julia,4.94,951
+binary-trees,Julia,7.28,634
+reverse-complement,Julia,1.44,522
+pidigits,Julia,0.97,506
+regex-redux,Julia,1.74,759
 """
 
 using CSV
