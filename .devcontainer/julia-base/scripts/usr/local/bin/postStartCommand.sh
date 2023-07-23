01e8dd1701aab0acf46df14adf976f3c1b0a7cba
Olivier Benz <olivier.benz@b-data.ch>
2023-07-23 16:25:50 +0200

    Dev Containers: Update postStartCommand.sh

--- .devcontainer/julia-base/scripts/usr/local/bin/postStartCommand.sh
+++ .devcontainer/julia-base/scripts/usr/local/bin/postStartCommand.sh
@@ -7,11 +7,11 @@ set -e
 mkdir -p "${HOME}/.julia/config"
 
 # Copy user-specific startup files if home directory is bind mounted
-if [ ! -f ".julia/config/startup_ijulia.jl" ]; then
+if [ ! -f "${HOME}/.julia/config/startup_ijulia.jl" ]; then
   cp -a /etc/skel/.julia/config/startup_ijulia.jl \
     "${HOME}/.julia/config"
 fi
-if [ ! -f ".julia/config/startup.jl" ]; then
+if [ ! -f "${HOME}/.julia/config/startup.jl" ]; then
   cp -a /etc/skel/.julia/config/startup.jl \
     "${HOME}/.julia/config"
 fi
